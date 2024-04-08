135b4496f8ca69273f591446b2c4d1ca6bb6352c
sm-dstoll <david.stoll@soulmachines.com>
2024-04-08 10:41:41 -0700

    create local dev branch

--- src/index.js
+++ src/index.js
@@ -1,37 +1,56 @@
-const functions = require('@google-cloud/functions-framework');
+// const functions = require('@google-cloud/functions-framework');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, (req, res) => {
+  console.log('listening')
+})
 
 async function executeHandler(req) {
   console.log('EXECUTE HANDLER');
   console.log({ req });
   const { text, memory, config } = req;
-  console.log({ text });
-  const pronounceList = config.pronounceList.split(' - ');
-  console.log({ pronounceList })
+  if (!config || !config.replacementList) return buildResponse(text, memory);
+  const replacementList = config.replacementList.split('\n');
   let newText = text;
-  pronounceList.forEach((phrase) => {
-    const [pre, post] = phrase.split(' : ');
-    const regex = new RegExp(pre, "ig")
-    newText = newText.replaceAll(regex, post)
-    console.log({ newText });
+  replacementList.forEach((phrase) => {
+    const [pre, post] = phrase.split(' - ');
+    const regex = new RegExp(`\\b${pre}\\b`, "ig");
+    newText = newText.replaceAll(regex, post);
   })
-  const variables = {};
+  console.log({ newText });
+  return buildResponse(newText, memory)
+}
 
-  const resp = {
+function buildResponse(text, memory) {
+  return {
     output: {
-      text: newText,
-      variables,
+      text,
     },
     memory,
     endConversation: false,
     endRouting: false,
   }
-  console.log({ resp })
-  return resp;
 }
 
-functions.http('smPronounce', async (request, response, next) => {
+app.post('/', async (request, response, next) => {
     const executeRequest = request.body;
+    console.log({ request });
     const executeResponse = await executeHandler(executeRequest);
     response.setHeader('Content-Type', 'application/json');
     response.send(executeResponse);
 });
+// functions.http('smPronounce', async (request, response, next) => {
+//     const executeRequest = request.body;
+//     const executeResponse = await executeHandler(executeRequest);
+//     response.setHeader('Content-Type', 'application/json');
+//     response.send(executeResponse);
+// });
+
+app.get('/', (req, res) => {
+  res.send('asdfasdfasdfas')
+})
